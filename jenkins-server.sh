bb9c97e3933a23a262c570aee2bc05bc7d47bfd7
shubham-rasal-123 <shubhamrasal861@gmail.com>
2026-07-22 19:38:27 +0530

    added jenkins server shell file

---
+++ jenkins-server.sh
@@ -0,0 +1,81 @@
+#!/bin/bash  
+
+#Jenkins Installation
+
+sudo apt update -y   
+sudo apt install fontconfig openjdk-21-jre -y 
+sudo java -version  
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+ 
+sudo apt update
+sudo apt install jenkins -y  
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+
+
+# Terraform Installation
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+
+sudo apt update -y
+sudo apt install terraform -y
+
+
+#Docker Installation
+
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+# Other installation
+
+usermod -aG docker jenkins
+apt install unzip -y
+
+
+# To install AWS CLI V2
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
+
+# To install maven
+
+apt update -y
+apt install maven -y
+
+sudo chmod 666 /var/run/docker.sock
+
+sudo systemctl restart jenkins

10249e388136f6926aa7eaf7c82ce8e379e7b6b4
shubham-rasal-123 <shubhamrasal861@gmail.com>
2026-07-22 19:15:44 +0530

    added data tf file to point zones and ec2

---
+++ data.tf
@@ -0,0 +1,18 @@
+data "available_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "latest" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}

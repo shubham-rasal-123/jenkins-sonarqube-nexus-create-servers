31ca01b3da8f592f407f2d36aa8d998b068f7043
shubham-rasal-123 <shubhamrasal861@gmail.com>
2026-07-22 20:01:38 +0530

    added modified networking file to add security group for nexus server

--- networking.tf
+++ networking.tf
@@ -218,3 +218,43 @@ resource "aws_security_group" "our-Security-Group-for-sonar" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+
+# AWS SECURITY GROUP FOR NEXUS SERVER
+
+resource "aws_security_group" "our-security-group-for-nexus" {
+  name        = "Our-Security-Group-for-nexus"
+  description = "Our Security Group"
+  vpc_id      = aws_vpc.our-vpc.id
+  tags = {
+    Name : "Our-Security-Group-for-nexus"
+    Environment : var.environment
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8081 # This is syntax to open port 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}

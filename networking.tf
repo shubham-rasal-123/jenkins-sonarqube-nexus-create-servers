1475102b1721d3cfaa6f60e17e125b92c255f2ce
shubham-rasal-123 <shubhamrasal861@gmail.com>
2026-07-22 19:50:58 +0530

    added modified networking tf file to add security group for sonar server

--- networking.tf
+++ networking.tf
@@ -179,3 +179,42 @@ resource "aws_security_group" "our-security-group" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+# AWS SECURITY GROUP FOR SONAR SERVER
+
+resource "aws_security_group" "our-Security-Group-for-sonar" {
+  name        = "Our-Security-Group-for-sonar"
+  description = "Our Security Group"
+  vpc_id      = aws_vpc.our-vpc.id
+  tags = {
+    Name : "Our-Security-Group-for-sonar"
+    Environment : var.environment
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9000 # This is syntax to open port 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
